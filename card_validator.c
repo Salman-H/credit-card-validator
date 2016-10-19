bea097534f0e1feffa5c20f452cfc69614a4a97b
Salman Hashmi <salman.hashmi2@mail.mcgill.ca>
2016-10-19 19:44:25 -0400

    add credit_validator file with program specs

---
+++ card_validator.c
@@ -0,0 +1,10 @@
+/**
+ * Prompts the user for a credit card number and then reports (via printf)
+ * whether it is a valid American Express, MasterCard, or Visa card number.
+ * @author Salman Hashmi
+ */
+ 
+ int main(void)
+ {
+	 return 0;
+ }

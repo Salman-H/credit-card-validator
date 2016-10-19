345461a1cfc73e45dd5307b0fa999e2de2dfc878
Salman Hashmi <salman.hashmi2@mail.mcgill.ca>
2016-10-19 19:50:51 -0400

    add function to compute length of a whole number

--- card_validator.c
+++ card_validator.c
@@ -7,7 +7,21 @@
 #include <cs50.h>
 #include <math.h>
 
+/* function prototypes */
+int get_length(long long num);
+
 int main(void)
 {
 	return 0;
+}
+
+/**
+ * Computes length of a whole number 
+ * @param num  a number of type long long
+ * @return length of num
+ */ 
+int get_length(long long num)
+{
+    // ceil returns the smallest int >= log10(num) e.g. ceil(log10(4510)) = ceil(3.6) = 4
+    return ceil(log10(num));
 }

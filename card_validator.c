b16b611fdb40bb9c0d3ca4835969f3fa3d6e4fc4
Salman Hashmi <salman.hashmi2@mail.mcgill.ca>
2016-10-20 01:35:31 -0400

    add function to compute first two digits of a number

--- card_validator.c
+++ card_validator.c
@@ -9,6 +9,7 @@
 
 /* function prototypes */
 int get_length(long long num);
+int get_identifier(long long num, int len);
 
 int main(void)
 {
@@ -24,4 +25,16 @@ int get_length(long long num)
 {
     // ceil returns the smallest int >= log10(num) e.g. ceil(log10(4510)) = ceil(3.6) = 4
     return ceil(log10(num));
+}
+
+/**
+ * computes the first 2 digits of a whole number
+ * @param num  a number of type long long
+ * @param len  length of this number of type int
+ * @return the first 2 digits of this number
+ */ 
+int get_identifier(long long num, int len)
+{
+    // Example:  4510 / 10^(4-2) = 45
+    return num / (long long) pow(10, len - 2);
 }
